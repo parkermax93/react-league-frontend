b45bf9f83cea41d8d3dac9cd20eadd73053cc2a6
parkermax93 <parker.williamson@gmail.com>
2020-10-08 17:01:55 -0700

    add like button

--- src/components/containers/ShowCharacter.jsx
+++ src/components/containers/ShowCharacter.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import CharacterCard from '../CharacterCard.jsx';
 import CharactersLink from '../CharactersLink.jsx'
+import LikeButton from '../LikeButton.jsx'
 
 const ShowCharacter = () => {
     return(
     <div className="CharacterCard">
         <CharacterCard />
+        <LikeButton />
         <CharactersLink />
     </div>
     )
