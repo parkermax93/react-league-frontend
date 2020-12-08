aee61c3a368879e1a77420274643c73e750e0f46
parkermax93 <parker.williamson@gmail.com>
2020-12-07 22:45:25 -0800

    Add search bar

--- src/components/containers/Characters.jsx
+++ src/components/containers/Characters.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import CharacterList from '../CharacterList.jsx';
 import CharacterCreate from '../CreateCharacter.jsx';
 import HomeButton from '../HomeButton.jsx';
+import SearchBar from '../SearchBar.jsx';
 
 const Characters = () => {
     return(
         <div className="App">
             <header className="App-header">
+                <SearchBar />
                 <CharacterList />
                 <CharacterCreate />
                 <HomeButton />
