9dddf8957e22d2de95fb62de13e6ea87e14b9080
parkermax93 <parker.williamson@gmail.com>
2020-12-07 22:41:04 -0800

    change linked to color to white

--- src/components/HomeButton.jsx
+++ src/components/HomeButton.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const HomeLink = () => {
     return (
-        <div>
+        <div className="Linked">
             <Link to="/">Home</Link>
         </div>
     )
