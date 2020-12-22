dc78449cdcec5f7c6644c5106c9969687f71d977
parkermax93 <parker.williamson@gmail.com>
2020-12-21 23:09:43 -0800

    remove unneccary code in reducer

--- src/components/NoMatch.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NoMatch = () => (
-        <div>
-            <h2>No Match Found</h2>
-        </div>
-    )
-
-export default NoMatch
--- src/components/containers/Characters.jsx
+++ src/components/containers/Characters.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import CharacterList from '../CharacterList.jsx';
 import CharacterCreate from '../CreateCharacter.jsx';
 import HomeButton from '../HomeButton.jsx';
-import SearchBar from '../SearchBar.jsx';
 
 const Characters = () => {
     return(
