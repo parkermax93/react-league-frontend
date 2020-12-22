ee2a285ef01906f632f03da637e7d8b92d0e71dd
parkermax93 <parker.williamson@gmail.com>
2020-12-21 23:16:45 -0800

    running sum of 1d array python answer

--- leetcode.rb
+++ leetcode.rb
@@ -11,3 +11,14 @@ def two_sum(nums, target)
     end
   end
 
+#Leetcode Running Sum of 1d Array Python
+class Solution(object):
+    def runningSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return [sum(nums[:i+1]) for i in range(len(nums))]
+
+    end
+end
