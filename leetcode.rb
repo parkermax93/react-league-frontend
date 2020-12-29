574adcb9298e87e6d57688fb20ca839f605e6de2
parkermax93 <parker.williamson@gmail.com>
2020-12-28 23:18:01 -0800

    Balance Leetcode

--- leetcode.rb
+++ leetcode.rb
@@ -92,3 +92,21 @@ def balanced_string_split(s)
   end
   total
 end
+#Count the Number of Consistent Strings Leetcode
+# @param {String} allowed
+# @param {String[]} words
+# @return {Integer}
+def count_consistent_strings(allowed, words)
+  allowed_chars = allowed.chars
+  count = 0
+  words.each do |word|
+      for i in 0...word.length
+          if !allowed_chars.include?(word[i])
+              count -= 1
+              break
+          end
+      end
+      count += 1
+  end
+  count
+end
