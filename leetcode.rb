79985e47767f196d6bbc5a896b88c7be7e872c59
parkermax93 <parker.williamson@gmail.com>
2020-12-28 23:10:38 -0800

    Sum Odd Length Subarrays Leetcode

--- leetcode.rb
+++ leetcode.rb
@@ -62,3 +62,17 @@ def restore_string(s, indices)
   return strings.join
 end
 
+#Sum of All Odd Length Subarrays
+# @param {Integer[]} arr
+# @return {Integer}
+def sum_odd_length_subarrays(arr)
+  count=0
+  for i in 0...arr.length
+      j = i
+      while j<arr.length
+          count+=arr[i..j].sum
+          j+=2
+      end
+  end
+  count
+end
