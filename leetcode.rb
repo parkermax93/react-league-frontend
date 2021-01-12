470d6630bae519f86042a2b35340d933f2f520f6
parkermax93 <parker.williamson@gmail.com>
2021-01-11 23:07:12 -0800

    Count Good Triplets Leetcode

--- leetcode.rb
+++ leetcode.rb
@@ -236,3 +236,20 @@ def number_of_matches(n)
   end
   count     
 end
+#Count Good Triplets
+# @param {Integer[]} arr
+# @param {Integer} a
+# @param {Integer} b
+# @param {Integer} c
+# @return {Integer}
+def count_good_triplets(arr, a, b, c)
+  count = 0
+  for i in 0...arr.length-2
+      for j in i+1...arr.length-1
+          for k in j+1...arr.length
+              count+=1 if (arr[i]-arr[j]).abs<=a and (arr[j]-arr[k]).abs<=b and (arr[i]-arr[k]).abs<=c
+          end
+      end
+  end
+  count
+end
