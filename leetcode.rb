ea17905b984fd753e39b3c964c50f050f472f119
parkermax93 <parker.williamson@gmail.com>
2021-02-01 23:22:34 -0800

    Count largest group leetcode

--- leetcode.rb
+++ leetcode.rb
@@ -358,3 +358,19 @@ def count_characters(words, chars)
   end
   p product
 end
+
+#Count Largest Group
+# @param {Integer} n
+# @return {Integer}
+def count_largest_group(n)
+  h = {}
+  number = ->x{ x.digits.sum}
+  (1..n).each do |i|
+      size = number[i]
+      h[size] ||= 0
+      h[size] += 1
+  end
+  values = h.values
+  max = values.max
+  values.count(max)
+end
