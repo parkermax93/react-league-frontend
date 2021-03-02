d8a0f1bcb7cba7d37e577b27b7817e87a22a32ff
parkermax93 <64799478+parkermax93@users.noreply.github.com>
2021-03-01 18:51:06 -0800

    Show another way reducer could work

--- src/reducers/myCharacters.jsx
+++ src/reducers/myCharacters.jsx
@@ -1,3 +1,26 @@
+// const initialState = {
+//     allCharacters: [],
+// }
+
+// export default (state = initialState, action) => {
+//     switch(action.type) {
+//         case "SET_ALL_CHARACTERS":
+//             return {
+//                 ...state,
+//                 // allCharacters: action.allCharacters
+//                 allCharacters: action.payload
+//             }
+//         default:
+//             return state
+//     }
+// }
+
+// export const setAllCharacters = allCharacters => {
+//     return {
+//         type: "SET_ALL_CHARACTERS",
+//         allCharacters
+//     }
+// }
 export default function characterReducer(
     state = { characters: [], loading: false }, action) {
         switch(action.type) {
