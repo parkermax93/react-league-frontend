de945c1eff3cb684185e347cf5c8b6c296f23dee
parkermax93 <64799478+parkermax93@users.noreply.github.com>
2021-03-15 19:50:37 -0700

    Sum of 1d Array Java Solution

--- leetcode.rb
+++ leetcode.rb
@@ -432,3 +432,12 @@ class Solution {
         return ans;
     }
 }
+#SUm of 1d Array (JAVA)
+class Solution {
+    public int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i] + nums[i - 1];
+        }
+        return nums;
+    }
+}
